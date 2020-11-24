760826b7741e4323b30289db48d3fe7e64b68fad
Kim jinwon <tmzkdl3753@naver.com>
2020-11-24 20:02:01 +0900

    asd

--- Makefile
+++ Makefile
@@ -1,26 +1,20 @@
 CC = arm-linux-gnueabi-gcc
 AR = arm-linux-gnueabi-ar
 
-all: libMyPeri.a ledtest buttontest buzzertest
+all: libMyPeri.a
 
-libMyPeri.a: button.o led.o buzzer.o
-	$(AR) rc libMyPeri.a led.o button.o buzzer.o 
+libMyPeri.a: button.o led.o buzzer.o fnd.o
+   $(AR) rc libMyPeri.a led.o button.o buzzer.o fnd.o
 
-button.o: button.h button.c
-	$(CC) button.c -o button.o -c
+   
+fnd.o: fnddrv.c fnddrv.h
+   $(CC) fnddrv.c -o fnd.o -c
 
-led.o: led.h led.c
-	$(CC) button.c -o button.o -c
-
-buzzer.o: buzzer.h buzzer.c
-	$(CC) buzzer.c -o buzzer.o -c
-
-
-buzzertest: buzzertest.c buzzer.h libMyPeri.a
-	$(CC) buzzertest.c -o buzzertest -l MyPeri -L.
-	scp buzzertest ecube@192.168.0.117:/home/ecube
+new_fndtest: fnddrvtest.c fnddrv.h libMyPeri.a
+   $(CC) fnddrvtest.c -l MyPeri -L. -o new_fndtest 
+   scp new_fndtest ecube@192.168.0.117:/home/ecube
 
 clean:
-	rm *,o
-	rm *.a
-	rm buttuntest
+   rm *.o
+   rm *.a
+   rm new_fndtest
---
+++ fnddrv.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/msg.h>
+#include <pthread.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include "fnddrv.h"
+
+#define FND_DRIVER_NAME "/dev/perifnd"
+
+int fndDisp(int num, int dotflag)
+{
+   int fd;
+   int temp,i;
+   stFndWriteForm stWriteData;
+   for (i=0; i<MAX_FND_NUM; i++)
+   {
+      stWriteData.DataDot[i] =(dotflag &(0x1 <<i)) ? 1: 0;
+      stWriteData.DataValid[i] = 1;
+   }
+   // if 6 fnd
+   temp = num % 1000000; stWriteData.DataNumeric[0] = temp/100000;
+   temp = num % 100000; stWriteData.DataNumeric[1] = temp/10000;
+   temp = num % 10000; stWriteData.DataNumeric[2] = temp/1000;
+   temp = num % 1000; stWriteData.DataNumeric[3] = temp/100;
+   temp = num % 100; stWriteData.DataNumeric[4] = temp/10;
+   stWriteData.DataNumeric[5] = num%10;
+   
+   fd = open(FND_DRIVER_NAME, O_RDWR);
+   if (fd<0)
+   {
+      perror("driver open error \n");
+      return 0;
+   }
+   write(fd, &stWriteData, sizeof(stFndWriteForm));
+   close(fd);
+   return 1;
+}
