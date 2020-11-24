a69836e6a3a81b2d5528e90acee42a21d347bf5f
Kim jinwon <tmzkdl3753@naver.com>
2020-11-24 22:13:23 +0900

    asd

--- Makefile
+++ Makefile
@@ -3,18 +3,17 @@ AR = arm-linux-gnueabi-ar
 
 all: libMyPeri.a
 
-libMyPeri.a: button.o led.o buzzer.o fnd.o
-   $(AR) rc libMyPeri.a led.o button.o buzzer.o fnd.o
+libMyPeri.a: button.o led.o buzzer.o fnd.o textlcd.o
+   $(AR) rc libMyPeri.a led.o button.o buzzer.o fnd.o textlcd.o
 
    
-fnd.o: fnddrv.c fnddrv.h
-   $(CC) fnddrv.c -o fnd.o -c
+textlcd.o: textlcddrv.c textlcddrv.h
+   $(CC) textlcddrv.c -o textlcd.o -c
 
-new_fndtest: fnddrvtest.c fnddrv.h libMyPeri.a
-   $(CC) fnddrvtest.c -l MyPeri -L. -o new_fndtest 
-   scp new_fndtest ecube@192.168.0.117:/home/ecube
+new_lcdtest: textlcdtest.c textlcddrv.h libMyPeri.a
+   $(CC) textlcdtest.c -l MyPeri -L. -o new_lcdtest 
+   scp new_lcdtest ecube@192.168.0.117:/home/ecube
 
 clean:
-   rm *.o
    rm *.a
-   rm new_fndtest
+   rm new_textlcd
---
+++ textlcddrv.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <linux/input.h>
+#include <sys/ioctl.h> 
+#include "textlcddrv.h"
+
+stTextLCD stlcd;
+static int fd = 0;
+static int len  = 0;
+
+int textinit(void){
+   fd = open(TEXTLCD_DRIVER_NAME, O_RDWR);
+   if(fd < 0){
+      perror("driver open error!\n");
+      return 1;
+   }
+   memset(&stlcd, 0,sizeof(stTextLCD));
+}
+
+int textwrite(char *str1, const char *str2)
+{
+   unsigned int linenum   = 0;
+   if(str1 < 3)
+   {
+      perror("args number is less than 2 \n");
+      return 1;
+   }
+   linenum = strtol(str1,  NULL, 10);
+   printf("linenum : %d\n",linenum);
+   if(linenum == 1){
+      stlcd.cmdData = CMD_DATA_WRITE_LINE_1;
+   }
+   else if(linenum == 2){
+      stlcd.cmdData = CMD_DATA_WRITE_LINE_2;
+   }
+   else{
+      printf("linenum : %d wrong range(1~2)\n",linenum);
+      return 1;
+   }
+   len = strlen(str2);
+   if(len >  COLUMN_NUM){
+      memcpy(stlcd.TextData[stlcd.cmdData - 1], str2, COLUMN_NUM);
+   }
+   else{
+      memcpy(stlcd.TextData[stlcd.cmdData - 1],str2,len);
+   }
+   stlcd.cmd = CMD_WRITE_STRING;
+   fd = open(TEXTLCD_DRIVER_NAME, O_RDWR);
+   if(fd < 0){
+      perror("driver open error!\n");
+      return 1;
+   }
+   write(fd, &stlcd,sizeof(stTextLCD));
+}
+
+int textexit(void){
+   close(fd);
+   return 0;
+}
